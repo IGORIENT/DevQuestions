cbc7c069404de8b740512ebba684e33f234b4d1f
IGORIENT <igor.bagaev.01@mail.ru>
2025-12-12 18:31:50 +0300

    designed Domain

---
+++ DevQuestyions/src/DevQuestions.Domain/Comments/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevQuestions.Domain.Comments
+{
+    internal class Comment
+    {
+        public Guid Id { get; set; }
+        public required Guid UserId { get; set; }
+        public Comment? Parent { get; set; }
+
+        public required Guid EntityId { get; set; }  
+        public List<Comment> Childrens { get; set; } = [];
+    }
+}
---
+++ DevQuestyions/src/DevQuestions.Domain/Reports/Report.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevQuestions.Domain.Reports;
+
+public class Report
+{
+    public Guid Id { get; set; }
+    public required Guid UserId { get; set; }
+
+    public required Guid ReportedUserId { get; set; }
+    public Guid? ResolvedByuserId { get; set; }
+    public required string Reason { get; set; }
+
+    public Status Status { get; set; } = Status.Open;
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+}
